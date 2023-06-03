fc36ea3c64fbf71aedc5f9f37787d8b7c62d0705
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-06-03 14:08:14 +0900

    Section 7: @ConfigurationProperties, @Profile

--- external-read-start/src/main/java/hello/ExternalReadApplication.java
+++ external-read-start/src/main/java/hello/ExternalReadApplication.java
@@ -1,9 +1,17 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV1;
+import hello.config.MyDataSourceConfigV3;
+import hello.config.MyDataSourceEnvConfig;
+import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDataSourceConfigV3.class)
+@ConfigurationPropertiesScan({"hello"})
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
